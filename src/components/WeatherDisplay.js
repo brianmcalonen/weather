84da22377fe493eeeecc41a7d4762fab0fc18be6
Brian McAlonen <bmcalonen@indeed.com>
2023-07-21 13:41:47 -0500

    daily chart

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SevenDayForecast from "./SevenDayForecast";
 import TodaysWeather from "./TodaysWeather";
+import DailyChart from "./DailyChart";
 
 const WeatherDisplay = ({ weather, locale }) => {
   return (
     <div className="">
       <TodaysWeather weather={weather} locale={locale} />
+      <DailyChart weather={weather} />
       <br />
       <SevenDayForecast weather={weather} />
     </div>
